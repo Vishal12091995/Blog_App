1555a32fad0fbed7e905ab7c8fce88f906fde21b
vishalgit <vishalda12@gmail.com>
2023-10-02 01:30:21 +0530

    second Commit

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,57 +1,90 @@
-//Blogging App using Hooks
-export default function Blog(){
-    
-    //Passing the synthetic event as argument to stop refreshing the page on submit
-    function handleSubmit(e){
-        e.preventDefault();
+import { useState,useRef,useEffect } from "react";
+
+export default function Blog() {
+  const [formData, setFormData] = useState({ title: "", content: "" });
+  const [blogs, setBlogs] = useState([]);
+  const titleRef= useRef(null);
+
+  useEffect(()=>{
+    titleRef.current.focus();
+  },[]);
+
+  useEffect(()=>{
+    if(blogs.length && blogs[0].title){
+        document.title=blogs[0].title;
+    }else{
+        document.title="NO Blogs!"
     }
+  },[blogs]);
 
-    return(
-        <>
-        {/* Heading of the page */}
-        <h1>Write a Blog!</h1>
+  function handleSubmit(e) {
+    e.preventDefault();
+    setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+    setFormData({ title: "", content: "" });
+    titleRef.current.focus();
+    console.log(blogs);
+  }
+  function removeBlog(i){
+    setBlogs(blogs.filter((blog,index)=>i!==index))
+  }
 
-        {/* Division created to provide styling of section to the form */}
-        <div className="section">
 
-        {/* Form for to write the blog */}
-            <form onSubmit={handleSubmit}>
+  return (
+    <>
+      <h1>Write a Blog!</h1>
+      <div className="section">
+        <form onSubmit={handleSubmit}>
+          <Row label="Title">
+            <input
+            required
+              className="input"
+              placeholder="Enter the Title of the Blog here.."
+              value={formData.title}
+              ref={titleRef}
+              onChange={(e) => {
+                setFormData({ ...formData, title: e.target.value });
+              }}
+            />
+          </Row>
 
-                {/* Row component to create a row for first input field */}
-                <Row label="Title">
-                        <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
-                </Row >
+          <Row label="Content">
+            <textarea
+            required
+              className="input"
+              placeholder="Content of the Blog goes here.."
+              value={formData.content}
+              onChange={(e) => {
+                setFormData({ ...formData, content: e.target.value });
+              }}
+            />
+          </Row>
 
-                {/* Row component to create a row for Text area field */}
-                <Row label="Content">
-                        <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."/>
-                </Row >
+          <button className="btn">ADD</button>
+        </form>
+      </div>
 
-                {/* Button to submit the blog */}            
-                <button className = "btn">ADD</button>
-            </form>
-                     
+      <hr />
+      <h2> Blogs </h2>
+      {blogs.map((blog, i) => (
+        <div className="blog" key={i}>
+          <h3>{blog.title}</h3>
+          <p>{blog.content}</p>
+          <div className="blog-btn">
+            <button onClick={()=>removeBlog(i)} className="btn remove">Delete</button>
+          </div>
         </div>
+      ))}
+    </>
+  );
+}
 
-        <hr/>
-
-        {/* Section where submitted blogs will be displayed */}
-        <h2> Blogs </h2>
-        
-        </>
-        )
-    }
-
-//Row component to introduce a new row section in the form
-function Row(props){
-    const{label} = props;
-    return(
-        <>
-        <label>{label}<br/></label>
-        {props.children}
-        <hr />
-        </>
-    )
+function Row(props) {
+  const { label } = props;
+  return (
+    <>
+      <label>{label}<br /></label>
+      {props.children}
+      <hr />
+    </>
+  );
 }
